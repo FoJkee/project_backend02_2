66017f9bd83f7912541f19fb378e352750787113
FoJkee <romanovsky0815@gmail.com>
2023-07-04 15:31:52 +0300

    start project and connect db

---
+++ src/db.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+import {MongoClient} from "mongodb";
+
+dotenv.config()
+
+
+const mongoUrl = process.env.Mongo_Url || "mongodb://127.0.0.1:27017"
+
+console.log(mongoUrl)
+
+if(!mongoUrl) throw new Error(' Not')
+
+
+const client = new MongoClient(mongoUrl)
+
+const db = client.db('social_network')
+
+
